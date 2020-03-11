0fb2ec77ba8a134f3d9a6a35eee27bfcea91603a
saranshnema <35158960+saranshnema@users.noreply.github.com>
2020-03-11 00:18:42 -0400

    Following changes have been made for Task 1- 1) CompensationController - A controller that provides get and post method for compensation 2) Compensation - This class provides structure for Compensation or how data will be handled 3) CompensationServiceImpl - This class implements how read and create will be performed for a given employee id and compensation respectively and data will be gathered to populate the Compensation object which is then returned to the requester 4) CompensationService - This is the interface for read and create operation by user 5) CompensationRepository - This interface provides method to retrieve all the compensation for a given employee id and helps maintain the data.

---
+++ src/main/java/com/mindex/challenge/data/Compensation.java
@@ -0,0 +1,60 @@
+package com.mindex.challenge.data;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Compensation {
+    private Employee employee;
+    private String employeeId;
+    private int salary;
+    private String effectiveDate;
+
+    public Compensation() {
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+        this.employeeId = this.employee.getEmployeeId();
+    }
+
+    @JsonIgnore
+    public String getEmployeeId() { return employeeId; }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String getEffectiveDate() {
+        return effectiveDate;
+    }
+
+    public void setEffectiveDate(String effectiveDate) throws ParseException {
+
+        this.effectiveDate = formatDate(effectiveDate);
+    }
+
+    private String formatDate(String effectiveDate) throws ParseException {
+
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date date = null;
+        try {
+            date = format.parse(effectiveDate);
+        } catch (ParseException e) {
+            throw new ParseException("This date cannot be parsed. Please enter a valid date-time string. Example - yyyy-MM-dd HH:mm:ss  (2020-03-10 23:59:59)", e.getErrorOffset());
+        }
+
+        return date.toString();
+    }
+
+}
