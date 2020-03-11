c2dc98ed6b6929c047304a2dc10b81c1aa9da6f8
saranshnema <35158960+saranshnema@users.noreply.github.com>
2020-03-10 20:53:49 -0400

    Following changes have been made for Task 1- 1) ReportingStructureController - A controller that provides get method 2) ReportingStructure - This class provides structure for ReportingStructure or how data will be handled 3) ReportingStructureServiceImpl - This class implements how read will be performed for a given employee id and data will be gathered to populate the Reporting Structure object which is then returned to the requester 4) ReportingStructureService - This is the interface for read operation by user 5) ReportingStructureUtilityService - This is the interface for reporting structure utility that gives method like getNumberOfReports 6) ReportingStructureUtilityImpl - This class implements the working of getNumberOfReports which takes in an employee and give the total number of direct reportee for that employee.

---
+++ src/main/java/com/mindex/challenge/utility/impl/ReportingStructureUtilityImpl.java
@@ -0,0 +1,37 @@
+package com.mindex.challenge.utility.impl;
+
+import com.mindex.challenge.data.Employee;
+import com.mindex.challenge.service.EmployeeService;
+import com.mindex.challenge.service.impl.ReportingStructureServiceImpl;
+import com.mindex.challenge.utility.ReportingStructureUtilityService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class ReportingStructureUtilityImpl implements ReportingStructureUtilityService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ReportingStructureUtilityImpl.class);
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @Override
+    public int getNumberOfReports(Employee employee) {
+        LOG.debug("Getting report count for employee with id [{}]", employee.getEmployeeId());
+        List<Employee> directReports = employee.getDirectReports();
+        if (directReports == null){
+            return 0;
+        }
+        int numberOfReports = directReports.size();
+
+        for(Employee reportees: directReports){
+            System.out.println(reportees.getEmployeeId());
+            Employee current_reportee = employeeService.read(reportees.getEmployeeId());
+            numberOfReports += getNumberOfReports(current_reportee);
+        }
+        return numberOfReports;
+    }
+}
